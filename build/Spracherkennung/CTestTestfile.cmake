219a1bdc3c9938ba1ed2d1be5133e26a3c68a73d
hagenfal <hagen.falkenstein@gmail.com>
2024-01-22 13:22:33 +0100

    first commit

---
+++ build/Spracherkennung/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hagen/Desktop/Robotik_Projekt/src/Spracherkennung
+# Build directory: /home/hagen/Desktop/Robotik_Projekt/build/Spracherkennung
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hagen/Desktop/Robotik_Projekt/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
